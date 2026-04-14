999c0fdbcbe430e6fa341b19510f546944d927aa
Z User <z@container>
2026-04-14 22:22:55 +0000

    fed48ce9-c0e7-4a1a-96f9-3ee576e500de

---
+++ temp-count-users.ts
@@ -0,0 +1,9 @@
+import { db } from './src/lib/db.ts'
+
+const users = await db.user.findMany({
+  where: { role: { in: ['STUDENT', 'PATIENT'] } },
+  select: { id: true, name: true, email: true, role: true }
+})
+
+console.log('Users count:', users.length)
+console.log('Users:', JSON.stringify(users, null, 2))
