ba1055f5cdc9a8ecb493f3fdee1408f7d59b21bd
Mohamed Abdelrahman <mohamed7744650@gmail.com>
2026-04-20 21:11:44 +0200

    first commit

--- START_SQLITE.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 > nul
-echo.
-echo Starting Smiley Dental Clinic (SQLite)...
-echo.
-if not exist db mkdir db
-echo.
-npm run dev
-pause
--- start-dev-fixed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-while true; do
-  echo "[$(date)] Starting dev server..."
-  bun run dev 2>&1
-  echo "[$(date)] Server stopped! Restarting in 3 seconds..."
-  sleep 3
-done
